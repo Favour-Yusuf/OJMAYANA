cc3570a046930980e2c81edd62155bb12148f22f
Favour-Yusuf <favouryusuf45@gmail.com>
2026-01-14 12:40:47 +0100

    done

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic"
+
+
 import PortfolioHero from "@/components/portfolio/PortfolioHero"
 import PortfolioGrid from "@/components/portfolio/PortfolioGrid"
 import PortfolioCTA from "@/components/portfolio/PortfolioCTA"
